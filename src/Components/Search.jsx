38068c1837276c8f821952328e6837f6e42304fb
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-02-05 11:04:32 +0530

    Add search input field to Search component

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,8 +1,16 @@
-
+import { FiSearch } from "react-icons/fi";
 const Search = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <div className=" text-center">
+        <input
+          className=" p-3 w-[40em] mt-16 shadow-lgfont-mono font-bold text-lg outline-none border-black border"
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
