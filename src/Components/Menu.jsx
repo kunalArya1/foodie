3802df6dd5a6e0e6e3b6ed64c030f8d161d2a564
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-02-12 22:46:18 +0530

    Add Link component to Menu.jsx

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,15 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { FaQuran } from "react-icons/fa";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const Menu = () => {
   const { id } = useParams();
-//   console.log(id);
+  //   console.log(id);
   const [MenuData, setMenuData] = useState([]);
-  const url =
-    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.252509430416556&lng=77.45797589421272&restaurantId=119238";
-
+  
   const getMenuData = async () => {
     const response = await axios.get(
       `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.252509430416556&lng=77.45797589421272&restaurantId=${id}`
@@ -25,8 +23,20 @@ const Menu = () => {
 
   if (MenuData.length === 0) return <h1> Loading ...</h1>;
   return (
-    <div className=" w-8/12 m-auto py-10 px-5">
+    <div className=" w-9/12 m-auto py-10 px-5">
       <h1 className=" font-bold text-4xl text-center">Menu Page</h1>
+      <br />
+      <p className=" text-lg font-semibold text-blue-500 h-12  bg-gray-200">
+        <Link to="/">/Restarutant</Link>/
+        <Link
+          to={`/resturnat/${MenuData?.data?.data?.cards[0]?.card?.card?.info?.id}`}
+        >
+          {MenuData?.data?.data?.cards[0]?.card?.card?.info?.name}
+        </Link>
+      </p>
+
+      <br />
+      <br />
       <br />
       <p className=" font-bold text-2xl ">
         {MenuData?.data?.data?.cards[0]?.card?.card?.info?.name}
