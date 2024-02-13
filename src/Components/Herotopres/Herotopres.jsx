cbfeba8c0b52d231cdf68e9656ac9a9b196115fa
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-02-13 09:54:21 +0530

    Add react-router-dom and implement link to restaurant details page

--- src/Components/Herotopres/Herotopres.jsx
+++ src/Components/Herotopres/Herotopres.jsx
@@ -2,6 +2,7 @@ import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
 import "./herotopres.css";
 import { useContext, useEffect, useRef, useState } from "react";
 import ResContext from "../../Context/ResContext";
+import { Link } from "react-router-dom";
 
 const Herotopres = () => {
   const [data, setdata] = useState([]);
@@ -70,38 +71,38 @@ const Herotopres = () => {
           className="h-[310px] my-4 w-full   flex gap-5 mb-9 overflow-x-auto overflow-hidden whitespace-nowrap border-b-2 "
         >
           {data.map((res) => (
-
-            <div
-              key={res.info.id}
-              className=" h-[180px] w-[270px] text-start  rounded-lg"
-            >
-             <div className="h-full w-full cursor-pointer ">
-                <img
-                  src={url + res.info.cloudinaryImageId}
-                  alt=""
-                  className="h-full w-full object-cover rounded-3xl "
-                />
-                <div className=" ml-5 mt-1">
-                  <h1 className="text-lg font-bold text-gray-600 leading-none overflow-auto">
-                    {res.info.name}
-                  </h1>
-                  <span className="  h-7 w-6 bg-green-700 rounded-full inline-block scale-[0.7] text-center">
-                    <span>⭐</span>
-                  </span>
-                  <span className=" font-semibold text-lg text-gray-600 ">
-                    {res.info.avgRating}.
-                  </span>
-                  <span className="font-semibold text-lg text-gray-600 ml-1 tracking-tighter">
-                    {res.info.sla.slaString}
-                  </span>
-                  <p className="  text-gray-500 text-[17px] overflow-auto">
-                    {res.info.cuisines.join(",")}
-                  </p>
-                  <p className="  text-gray-500 text-[18px]">
-                    {res.info.locality}
-                  </p>
+            <div key={res.info.id}>
+              <Link to={`/resturnat/${res.info.id}`}>
+                <div className=" h-[180px] w-[270px] text-start  rounded-lg">
+                  <div className="h-full w-full cursor-pointer ">
+                    <img
+                      src={url + res.info.cloudinaryImageId}
+                      alt=""
+                      className="h-full w-full object-cover rounded-3xl "
+                    />
+                    <div className=" ml-5 mt-1">
+                      <h1 className="text-lg font-bold text-gray-600 leading-none overflow-auto">
+                        {res.info.name}
+                      </h1>
+                      <span className="  h-7 w-6 bg-green-700 rounded-full inline-block scale-[0.7] text-center">
+                        <span>⭐</span>
+                      </span>
+                      <span className=" font-semibold text-lg text-gray-600 ">
+                        {res.info.avgRating}.
+                      </span>
+                      <span className="font-semibold text-lg text-gray-600 ml-1 tracking-tighter">
+                        {res.info.sla.slaString}
+                      </span>
+                      <p className="  text-gray-500 text-[17px] overflow-auto">
+                        {res.info.cuisines.join(",")}
+                      </p>
+                      <p className="  text-gray-500 text-[18px]">
+                        {res.info.locality}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </Link>
             </div>
           ))}
         </div>
--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -7,40 +7,41 @@ const Menu = () => {
   const { id } = useParams();
   //   console.log(id);
   const [MenuData, setMenuData] = useState([]);
-  
+
   const getMenuData = async () => {
     const response = await axios.get(
       `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.252509430416556&lng=77.45797589421272&restaurantId=${id}`
     );
-    // MenuData?.data?.data?.cards[0]?.card?.card?.info?.name
-    setMenuData(response);
+
+    console.log(response);
+    const ArrayVlaue = [];
+
+    ArrayVlaue.push(response?.data?.data?.cards[0]?.card?.card?.info);
+
+    console.log(ArrayVlaue);
+
+    setMenuData(ArrayVlaue);
   };
-  //   console.log(MenuData);
 
   useEffect(() => {
     getMenuData();
   }, []);
 
   if (MenuData.length === 0) return <h1> Loading ...</h1>;
+
   return (
     <div className=" w-9/12 m-auto py-10 px-5">
-      <h1 className=" font-bold text-4xl text-center">Menu Page</h1>
-      <br />
-      <p className=" text-lg font-semibold text-blue-500 h-12  bg-gray-200">
-        <Link to="/">/Restarutant</Link>/
-        <Link
-          to={`/resturnat/${MenuData?.data?.data?.cards[0]?.card?.card?.info?.id}`}
-        >
-          {MenuData?.data?.data?.cards[0]?.card?.card?.info?.name}
+      <p className="text-xs">
+        <Link to="/">Home</Link> &nbsp; / &nbsp;
+        <Link to={`/resturnat/${MenuData?.[0]?.id}`}>
+          {MenuData?.[0]?.name}
         </Link>
       </p>
 
       <br />
       <br />
       <br />
-      <p className=" font-bold text-2xl ">
-        {MenuData?.data?.data?.cards[0]?.card?.card?.info?.name}
-      </p>
+      <p className=" font-bold text-2xl ">{MenuData?.[0]?.name}</p>
     </div>
   );
 };
