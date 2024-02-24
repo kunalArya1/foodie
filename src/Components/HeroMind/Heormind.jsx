893ea4cb1be82a7038fb4a8b9f5f426c3afd4199
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2024-02-24 21:37:47 +0530

    Fix loading state in Heormind component

--- src/Components/HeroMind/Heormind.jsx
+++ src/Components/HeroMind/Heormind.jsx
@@ -14,7 +14,7 @@ const Heormind = (props) => {
     );
   }, [resData]);
 
-  // if (!resMindData) return <h1>Loading...</h1>;
+  if (!resMindData) return <h1>Loading...</h1>;
 
   const url =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
